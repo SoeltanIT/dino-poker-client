63d80dbddad62920f323a978e286560127537c4e
jeffryAnderson183 <ajeffry183@gmail.com>
2025-09-25 14:29:09 +0700

    comment tab switcher

--- src/components/organisms/Profile/TransactionHistory/index.tsx
+++ src/components/organisms/Profile/TransactionHistory/index.tsx
@@ -253,6 +253,7 @@ export default function TransactionHistoryPage({
         </div>
       </div>
 
+      {/* <TabSwitcher tabs={tabs} activeTab={activeTab} onTabChange={setActiveTab} /> */}
 
       {/* Mobile View */}
       {activeTab === 'fiat' && (
