d4812f549b1fd18198385709950b5addf612a5be
jeffryAnderson183 <ajeffry183@gmail.com>
2025-10-20 18:41:59 +0700

    fix

--- src/components/organisms/Promotion/DetailPromotion/types.d.ts
+++ src/components/organisms/Promotion/DetailPromotion/types.d.ts
@@ -3,7 +3,7 @@ import { LangProps } from '@/types/langProps'
 import { PromotionDTO } from '@/types/promotionDTO'
 
 export interface DetailPromotionProps {
-  lang?: LangProps
+  lang: LangProps
   locale?: Locale
   initialData: PromotionDTO
   isLogin?: boolean
