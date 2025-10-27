58b474a48e99c5836ea32d13afbca78c1c0d6796
jeanfe21 <ajeffry183-9002@vercel.com>
2025-10-27 15:48:45 +0700

    fix: flags

--- src/app/.well-known/vercel/flags/route.ts
+++ src/app/.well-known/vercel/flags/route.ts
@@ -1,9 +1,4 @@
-import { createFlagsDiscoveryEndpoint, getProviderData, KeyedFlagDefinitionType } from 'flags/next'
+import { createFlagsDiscoveryEndpoint, getProviderData } from 'flags/next'
 import * as flags from '../../../../../flags'
-/**
- * This route sets up the Flags Explorer
- * https://vercel.com/docs/feature-flags/flags-explorer/getting-started#creating-the-flags-api-endpoint
- */
-export const GET = createFlagsDiscoveryEndpoint(() =>
-  getProviderData(flags as unknown as Record<string, KeyedFlagDefinitionType | readonly unknown[]>)
-)
+
+export const GET = createFlagsDiscoveryEndpoint(() => getProviderData(flags))
