0bc2eacb2d116416ff754a1acd8e5211d0e42026
do-zoo <edwardedo603@gmail.com>
2025-11-03 12:00:41 +0700

    Update next.config.mjs to include new CDN hostname and adjust GameCard component for responsive image sizes

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'staging-agg-storage.s3.ap-southeast-1.amazonaws.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.agg.gg'
       }
     ]
   }
