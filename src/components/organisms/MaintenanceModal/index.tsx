f1a4638e00b2dea15e48c988c90a52d9361a701b
jeffryAnderson183 <ajeffry183@gmail.com>
2025-11-09 18:04:31 +0700

    fix: updated navbar

--- src/components/organisms/MaintenanceModal/index.tsx
+++ src/components/organisms/MaintenanceModal/index.tsx
@@ -1,6 +1,5 @@
 import { Dialog, DialogClose, DialogContent, DialogHeader } from '@/components/ui/dialog'
 import { DialogProps } from '@radix-ui/react-dialog'
-import { XIcon } from 'lucide-react'
 import Image from 'next/image'
 
 interface MaintenanceModalProps extends DialogProps {
@@ -12,9 +11,7 @@ export function MaintenanceModal({ label, ...props }: MaintenanceModalProps) {
     <Dialog {...props}>
       <DialogContent className='bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-md'>
         <DialogHeader>
-          <DialogClose className='absolute top-4 right-4'>
-            <XIcon className='w-4 h-4' />
-          </DialogClose>
+          <DialogClose />
         </DialogHeader>
         <div className='flex flex-col items-center justify-center py-8 px-4'>
           <Image
